37190ad8784e10d504b75911177c8ea3707d3feb
Elijah <elijahoneal@yahoo.com>
2021-05-15 21:43:29 -0400

    NPM scripts + index + server setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const server = express();
-
+const actionRouter = require('./actions/actions-router')
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use(express.json())
+server.use('/api/action', actionRouter)
 
 module.exports = server;
